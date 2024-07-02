ef9f867df06268f8603df229a0da5c99f48b2e04
kevin-chela <kevinchela714@gmail.com>
2024-07-02 22:52:07 +0200

    Set up routing

---
+++ src/components/dashboard/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div>
+       <h5>Hello Default Route & Page</h5>
+    </div>
+  )
+}
+
+export default index
---
+++ src/routing.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Dashboard from './components/dashboard/index'
+
+import StoreOutlinedIcon from '@mui/icons-material/StoreOutlined';
+
+const routing = [
+    {
+      type: "collapse",
+      name: "Dashboard",
+      key: "dashboard",
+      route: "/",
+      icon: <StoreOutlinedIcon size="12px" />,
+      component: <Dashboard />,
+      noCollapse: true,
+    },
+    // Add more routes as needed
+  ];
+
+export default routing
